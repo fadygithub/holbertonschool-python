3643ace432c9077fe1425902eb739de22846b83a
fadygithub <fadyhanna1@outlook.com>
2021-01-21 14:27:04 +0000

    Debug the file 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,31 +1,22 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-import sys
-
-def main():
-    arguments = sys.argv
-    agruments = arguments[1:]
-
-    if len(arguments) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-
-    elif arguments[2] != "+" and arguments[2] != "-" and arguments[2] != "*" and arguments[2] != "/":
-        print("Unknown operator. Available operators: +, -, * and /")
-        print("**")
-        sys.exit(1)
-    
-    elif arguments[2] == "+":
-        print('{} {} {} = {}'.format(arguments[1], arguments[2], arguments[3], format(add(int(arguments[1]), int(arguments[3])))))
-
-    elif arguments[2] == "-":
-        print('{} {} {} = {}'.format(arguments[1], arguments[2], arguments[3], format(sub(int(arguments[1]), int(arguments[3])))))
-
-    elif arguments[2] == "*":
-        print('{} {} {} = {}'.format(arguments[1], arguments[2], arguments[3], format(mul(int(arguments[1]), int(arguments[3])))))
-
-    elif arguments[2] == "/":
-        print('{} {} {} = {}'.format(arguments[1], arguments[2], arguments[3], format(div(int(arguments[1]), int(arguments[3])))))
-
-if __name__ == '__main__':
-    main()
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+    if len(sys.argv) != 4:
+        sys.stderr.write('Usage: ./100-my_calculator.py <a> <operator> <b>\n')
+        exit(1)
+    operator = sys.argv[2]
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    if str(operator) not in("+", "-", "*", "/"):
+        sys.stderr.write('Unknown operator. Available \
+        operators: +, -, * and /\n')
+        exit(1)
+    elif str(operator) == "+":
+        print("{} {} {} = {}".format(a, operator, b, add(a, b)))
+    elif str(operator) == "-":
+        print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
+    elif str(operator) == "/":
+        print("{} {} {} = {}".format(a, operator, b, div(a, b)))
+    else:
+        print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
