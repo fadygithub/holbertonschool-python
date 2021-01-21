3ff0cf095ea2e8dd3052ac55c7224573d6f7bfa7
fadygithub <fadyhanna1@outlook.com>
2021-01-21 14:29:47 +0000

    Debug the file 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv, exit
+
+
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
-    import sys
-    if len(sys.argv) != 4:
-        sys.stderr.write('Usage: ./100-my_calculator.py <a> <operator> <b>\n')
+    if len(argv) != 4:
+        print('Usage: ./100-my_calculator.py <a> <operator> <b>')
         exit(1)
-    operator = sys.argv[2]
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    if str(operator) not in("+", "-", "*", "/"):
-        sys.stderr.write('Unknown operator. Available \
-        operators: +, -, * and /\n')
-        exit(1)
-    elif str(operator) == "+":
-        print("{} {} {} = {}".format(a, operator, b, add(a, b)))
-    elif str(operator) == "-":
-        print("{} {} {} = {}".format(a, operator, b, sub(a, b)))
-    elif str(operator) == "/":
-        print("{} {} {} = {}".format(a, operator, b, div(a, b)))
     else:
-        print("{} {} {} = {}".format(a, operator, b, mul(a, b)))
+        a = int(argv[1])
+        b = int(argv[3])
+        if argv[2] == "+":
+            print('{:d} + {:d} = {:d}'.format(a, b, add(a, b)))
+        elif argv[2] == '-':
+            print('{:d} - {:d} = {:d}'.format(a, b, sub(a, b)))
+        elif argv[2] == '*':
+            print('{:d} * {:d} = {:d}'.format(a, b, mul(a, b)))
+        elif argv[2] == '/':
+            print('{:d} / {:d} = {:d}'.format(a, b, div(a, b)))
+        else:
+            print('Unknown operator. Available operators: +, -, * and /')
+            exit(1)
