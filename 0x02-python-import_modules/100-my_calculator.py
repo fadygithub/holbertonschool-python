be8c916c850998a926a82e5d1dab2ef87cc1d0f5
fadygithub <fadyhanna1@outlook.com>
2021-01-21 14:23:19 +0000

    Debug the file 100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -10,16 +10,21 @@ def main():
     if len(ar) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
+
     elif ar[2] != "+" and ar[2] != "-" and ar[2] != "*" and ar[2] != "/":
         print("Unknown operator. Available operators: +, -, * and /")
         print("**")
         sys.exit(1)
+    
     elif ar[2] == "+":
         print('{} {} {} = {}'.format(ar[1], ar[2], ar[3], format(add(int(ar[1]), int(ar[3])))))
+
     elif ar[2] == "-":
         print('{} {} {} = {}'.format(ar[1], ar[2], arguments[3], format(sub(int(ar[1]), int(ar[3])))))
+
     elif ar[2] == "*":
         print('{} {} {} = {}'.format(ar[1], ar[2], arguments[3], format(mul(int(ar[1]), int(ar[3])))))
+
     elif ar[2] == "/":
         print('{} {} {} = {}'.format(ar[1], ar[2], arguments[3], format(div(int(ar[1]), int(ar[3])))))
 
