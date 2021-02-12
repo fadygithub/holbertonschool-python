db87e17e755fb1534adb8021f0d7f4e314cf24a9
fadygithub <fadyhanna1@outlook.com>
2021-02-12 15:21:48 +0000

    Debug the file 0-lockboxes.py

--- 0x0A-lockboxes/0-lockboxes.py
+++ 0x0A-lockboxes/0-lockboxes.py
@@ -11,3 +11,26 @@ def canUnlockAll(boxes):
     if len(keys_list) != len(boxes):
         return False
     return True
+#!/usr/bin/python3
+
+
+def canUnlockAll(boxes):
+    keys = [0]
+    for key in keys:
+        for x in boxes[key]:
+            if x < len(boxes):
+                if x not in keys:
+                    keys.append(x)
+    if len(keys) != len(boxes):
+        return False
+    return True
+'''
+        if boxes[key] != -999:
+            keys += boxes[key]
+            boxes[key] = -999
+
+    for x in boxes:
+        if x != -999:
+            return False
+    return True
+'''
