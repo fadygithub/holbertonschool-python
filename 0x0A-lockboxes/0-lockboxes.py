e57a4e26b8ce1203c469dc67eaabaf8929565329
fadygithub <fadyhanna1@outlook.com>
2021-02-12 15:27:01 +0000

    Debig the file 0-lockboxes.py

--- 0x0A-lockboxes/0-lockboxes.py
+++ 0x0A-lockboxes/0-lockboxes.py
@@ -2,19 +2,6 @@
 
 
 def canUnlockAll(boxes):
-    keys_list = [0]
-    for k in keys_list:
-        for x in boxes[k]:
-            if x < len(boxes):
-                if x not in keys_list:
-                    keys.append(x)
-    if len(keys_list) != len(boxes):
-        return False
-    return True
-#!/usr/bin/python3
-
-
-def canUnlockAll(boxes):
     keys = [0]
     for key in keys:
         for x in boxes[key]:
