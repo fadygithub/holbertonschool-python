628d56b98c2d48b3cb2b6fd30395a1c5c1e07a74
fadygithub <fadyhanna1@outlook.com>
2021-02-26 15:55:46 +0000

    Debug 0-body_size.sh

--- 0x0F-python-network_0/0-body_size.sh
+++ 0x0F-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
-#!/bin/bash
-# Receives URL -> sends a request to that URL -> Then it will display size of the response body
+#!/bin/bash/
+# Receives URL,then it will display size of the response
 curl -sI "$1" | grep Content-Length | cut -d' ' -f2
