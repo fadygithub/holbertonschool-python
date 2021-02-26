63881b45b99160013e9dc6f90a73184e34bcc4ea
fadygithub <fadyhanna1@outlook.com>
2021-02-26 16:56:25 +0000

    debug the file 102-catch_me.sh

--- 0x0F-python-network_0/102-catch_me.sh
+++ 0x0F-python-network_0/102-catch_me.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # sends a request to 0.0.0.0:5000/catch_me which will return the server's response
 # curl -s -L -H "Origin: HolbertonSchool" -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
-curl -sLX PUT -H "origin:HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+curl -s -L -X -H "Origin: HolbertonSchool" -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me
