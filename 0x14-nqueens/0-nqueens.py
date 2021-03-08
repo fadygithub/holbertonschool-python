9d85227e06f61dce089cdb66d8723a3ee98ac17f
fadygithub <fadyhanna1@outlook.com>
2021-03-08 16:47:33 +0000

    Debug the file  0-nqueens.py

--- 0x14-nqueens/0-nqueens.py
+++ 0x14-nqueens/0-nqueens.py
@@ -1,69 +1,45 @@
 #!/usr/bin/python3
+""""
+0-nqueens.py
 """
-    0-nqueens.py Task
-"""
-import sys
 
+from sys import argv, exit
 
-def format(board):
-    """Print board"""
-    r = []
-    for x in range(len(board)):
-        colIdx = board[x].index(1)
-        r.append([x, colIdx])
-    print(r)
-
-
-def is_Valid(board, column, row, n):
-    """We check if the location is a valid queen"""
-    # Check the previous columns
-    for x in range(column):
-        if board[row][x] == 1:
-            return False
-    # Check for the upper diagonal
-    r = row
-    c = column
-    while r >= 0 and c >= 0:
-        if board[r][c] == 1:
-            return False
-        r -= 1
-        c -= 1
-    # Check for the lower diagonal
-    r = row
-    c = column
-    while r < n and c >= 0:
-        if board[r][c] == 1:
-            return False
-        r += 1
-        c -= 1
-    return True
-
-
-def Queens(board, column, n):
-    """Place our queen in all the
-     positions of the board recursivly"""
-    flag = False
-    if column == n:
-        format(board)
-        return True
-    for row in range(0, n):
-        if is_Valid(board, column, row, n):
-            board[row][column] = 1
-            flag = Queens(board, column + 1, n) or flag
-            board[row][column] = 0
-    return flag
-
-
-if __name__ == '__main__':
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print('Usage: nqueens N')
         exit(1)
-    if not sys.argv[1].isdigit():
-        print("N must be a number")
+    try:
+        n = int(argv[1])
+    except BaseException:
+        print('N must be a number')
         exit(1)
-    n = int(sys.argv[1])
+
     if n < 4:
-        print("N must be at least 4")
+        print('N must be at least 4')
         exit(1)
-    board = [[0 for i in range(n)]for j in range(n)]
-    nQueens(board, 0, n)
+
+    sol = []
+
+    def queens(row, n, sol):
+        if row == n:
+            print(sol)
+        else:
+            for col in range(n):
+                place = [row, col]
+                if valid_placement(sol, place):
+                    sol.append(place)
+                    queens(row + 1, n, sol)
+                    sol.remove(place)
+
+    def valid_placement(sol, place):
+        for queen in sol:
+            if queen[1] == place[1]:
+                return False
+            if queen[0] + queen[1] == place[0] + place[1]:
+                return False
+            if queen[0] - queen[1] == place[0] - place[1]:
+                return False
+        return True
+
+    queens(0, n, sol)
