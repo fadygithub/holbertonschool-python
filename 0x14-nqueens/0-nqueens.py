f98b86083dc7fc7c9740e55e93db09ba022a0e5b
fadygithub <fadyhanna1@outlook.com>
2021-03-08 16:37:06 +0000

    Debug the file  0-nqueens.py

--- 0x14-nqueens/0-nqueens.py
+++ 0x14-nqueens/0-nqueens.py
@@ -1,38 +1,69 @@
 #!/usr/bin/python3
 """
-    0-nqueens.py
+    0-nqueens.py Task
 """
-
-from sys import argv
-
-if len(argv) > 2:
-    print("Usage: nqueens N")
-    exit(1)
-try:
-    N = int(argv[1])
-except ValueError:
-    print("N must be a number")
-    exit(1)
-if N < 4:
-    print("N must be at least 4")
-    exit(1)
+import sys
 
 
-def attack(col, queens):
-    return col in queens or \
-           any(abs(col - x) == len(queens)-i for i, x in enumerate(queens))
+def format(board):
+    """Print board"""
+    r = []
+    for x in range(len(board)):
+        colIdx = board[x].index(1)
+        r.append([x, colIdx])
+    print(r)
 
 
-def solve(n):
-    solutions = [[]]
-    for r in range(n):
-        solutions = [solution+[i+1]
-                     for solution in solutions
-                     for i in range(BOARD_SIZE)
-                     if not attack(i+1, solution)]
-    return solutions
+def is_Valid(board, column, row, n):
+    """We check if the location is a valid queen"""
+    # Check the previous columns
+    for x in range(column):
+        if board[row][x] == 1:
+            return False
+    # Check for the upper diagonal
+    r = row
+    c = column
+    while r >= 0 and c >= 0:
+        if board[r][c] == 1:
+            return False
+        r -= 1
+        c -= 1
+    # Check for the lower diagonal
+    r = row
+    c = column
+    while r < n and c >= 0:
+        if board[r][c] == 1:
+            return False
+        r += 1
+        c -= 1
+    return True
+
+
+def Queens(board, column, n):
+    """Place our queen in all the
+     positions of the board recursivly"""
+    flag = False
+    if column == n:
+        format(board)
+        return True
+    for row in range(0, n):
+        if is_Valid(board, column, row, n):
+            board[row][column] = 1
+            flag = Queens(board, column + 1, n) or flag
+            board[row][column] = 0
+    return flag
+
 
 if __name__ == '__main__':
-    BOARD_SIZE = int(argv[1])
-    for ans in solve(BOARD_SIZE):
-        print(list([x-1, y-1] for x, y in enumerate(ans, start=1)))
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        exit(1)
+    if not sys.argv[1].isdigit():
+        print("N must be a number")
+        exit(1)
+    n = int(sys.argv[1])
+    if n < 4:
+        print("N must be at least 4")
+        exit(1)
+    board = [[0 for i in range(n)]for j in range(n)]
+    nQueens(board, 0, n)
