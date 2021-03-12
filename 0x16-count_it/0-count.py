0d8a3b8295685f9d8bfdc8171d8235baa9f4c532
fadygithub <fadyhanna1@outlook.com>
2021-03-12 17:01:22 +0000

    Debug the file 0-count.py

--- 0x16-count_it/0-count.py
+++ 0x16-count_it/0-count.py
@@ -13,26 +13,26 @@ import requests
 
 def count_words(subreddit, word_list, res=defaultdict(int), after=None):
     ''' Counting the words in subreddit '''
-    ag = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6)\
+    agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6)\
             AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132i\
             Safari/537.36"
-    headers = {"User-Agent": ag}
+    headers = {"User-Agent": agent}
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     if after:
         url += '?after={}'.format(after)
     try:
         r = requests.get(url, headers=headers, allow_redirects=False).json()
         titles = r.get('data').get('children')
-        for x in titles:
-            c = Counter(x.get('data').get('title').lower().split(' '))
-            for wrd in word_list:
-                if wrd.lower() in c:
-                    res[wrd] += c.get(wrd.lower())
+        for t in titles:
+            c = Counter(t.get('data').get('title').lower().split(' '))
+            for word in word_list:
+                if word.lower() in c:
+                    res[word] += c.get(word.lower())
         after = r.get('data').get('after')
         if after:
             return count_words(subreddit, word_list, res, after)
-        sort_first = sorted(res.items(), key=lambda x: x[0])
-        for k, v in sorted(sort_first, key=lambda x: x[1], reverse=True):
+        first_sort = sorted(res.items(), key=lambda x: x[0])
+        for k, v in sorted(first_sort, key=lambda x: x[1], reverse=True):
             print('{}: {}'.format(k, v))
     except:
         return
