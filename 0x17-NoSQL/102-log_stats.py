45136c5b0f8c6d1a637d79f7a36d6aa52215b63b
fadygithub <fadyhanna1@outlook.com>
2021-03-16 18:43:15 +0000

    Debug the file 102-log_stats.py

--- 0x17-NoSQL/102-log_stats.py
+++ 0x17-NoSQL/102-log_stats.py
@@ -1,21 +1,22 @@
 #!/usr/bin/env python3
-"""Log statistics"""
-
+"""
+Using PyMongo in order to parse the logs
+"""
 
 from pymongo import MongoClient
 
 
 def log_stats():
-    """log statistics"""
+    """Function for parsing logs"""
     client = MongoClient('mongodb://127.0.0.1:27017')
-    l = client.logs.nginx
-    total = l.count_documents({})
-    get = l.count_documents({"method": "GET"})
-    post = l.count_documents({"method": "POST"})
-    put = l.count_documents({"method": "PUT"})
-    patch = l.count_documents({"method": "PATCH"})
-    delete = l.count_documents({"method": "DELETE"})
-    path = l.count_documents(
+    logs_collection = client.logs.nginx
+    total = logs_collection.count_documents({})
+    get = logs_collection.count_documents({"method": "GET"})
+    post = logs_collection.count_documents({"method": "POST"})
+    put = logs_collection.count_documents({"method": "PUT"})
+    patch = logs_collection.count_documents({"method": "PATCH"})
+    delete = logs_collection.count_documents({"method": "DELETE"})
+    path = logs_collection.count_documents(
         {"method": "GET", "path": "/status"})
     print(f"{total} logs")
     print("Methods:")
@@ -26,15 +27,15 @@ def log_stats():
     print(f"\tmethod DELETE: {delete}")
     print(f"{path} status check")
     print("IPs:")
-    sorted_ips = l.aggregate(
+    ip = logs_collection.aggregate(
         [{"$group": {"_id": "$ip", "count": {"$sum": 1}}},
          {"$sort": {"count": -1}}])
-    i = 0
-    for sort in sorted_ips:
-        if x == 10:
+    c = 0
+    for x in ip:
+        if c == 10:
             break
-        print(f"\t{sort.get('_id')}: {sort.get('count')}")
-        x += 1
+        print(f"\t{x.get('_id')}: {x.get('count')}")
+        c += 1
 
 
 if __name__ == "__main__":
